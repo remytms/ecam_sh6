9c13d7db026a110affede3e78cbc411dbd9cebfb
Rémy Taymans <remytaymans@gmail.com>
2016-11-22 15:15:47 +0100

    Create template for all cmd

---
+++ cmd/mypwd.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2016 Rémy Taymans <14291@student.ecam.be>
+ *                Louis Hugues Randriamora <12413@student.ecam.be>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+int main(int argc, char *argv[])
+{
+    /*
+     * Complete code here.
+     */
+}
+
