54104d8a6d5b683fe2068d5b1529c472ea5445ec
Rémy Taymans <remytaymans@gmail.com>
2016-11-23 18:01:48 +0100

    Implement uptime

--- cmd/myuptime.c
+++ cmd/myuptime.c
@@ -20,13 +20,25 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "myuptimelib.h"
+
+/*
+ * See also:
+ *     man 2 sysinfo
+ *     man 5 utmp
+ *     man 3 getutent
+ */
 int main(int argc, char *argv[])
 {
     int pretty_flag = 0;
     int since_flag = 0;
 
-    int c;
+    int c, nbr_user;
     char *pgr_name;
+    char *now_time;
+    char *uptime;
+    char *load;
+    char *since;
     struct option options[] = 
     {
         {"pretty", no_argument, NULL, 'p'},
@@ -51,9 +63,58 @@ int main(int argc, char *argv[])
         }
     }
 
-    /*
-     * Complete code here.
-     */
+    if (since_flag) {
+        since = myuptime_get_start_date();
+        if (since == NULL) {
+            perror(pgr_name);
+            exit(EXIT_FAILURE);
+        }
+        printf("%s\n", since);
+        free(since);
+        exit(EXIT_SUCCESS);
+    }
+
+    uptime = myuptime_get_uptime(pretty_flag);
+    if (uptime == NULL) {
+        perror(pgr_name);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pretty_flag) {
+        printf("%s\n", uptime);
+        free(uptime);
+        exit(EXIT_SUCCESS);
+    }
+
+    now_time = myuptime_get_time();
+    if (now_time == NULL) {
+        perror(pgr_name);
+        exit(EXIT_FAILURE);
+    }
+
+    nbr_user = myuptime_count_connected_users();
+    if (nbr_user == -1) {
+        perror(pgr_name);
+        exit(EXIT_FAILURE);
+    }
+
+    load = myuptime_get_load();
+    if (load == NULL) {
+        perror(pgr_name);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%s ", now_time);
+
+    printf("up %s, ", uptime);
+
+    printf("%d user(s), ", nbr_user);
+
+    printf("load average: %s\n", load);
+
+    free(now_time);
+    free(uptime);
+    free(load);
 
     return EXIT_SUCCESS;
 }
